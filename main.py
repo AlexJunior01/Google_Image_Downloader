d5f4b2b7315ad52e27ad6ceb3ab0fc5b58022d8d
Alex Junior <alexmomijunior@gmail.com>
2021-01-29 13:31:53 -0300

    bugfix ordem de download

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+# Bibliotecas utilizadas no scrap
 import bs4
 from selenium import webdriver
 import sys
 import os
 import time
+
+# Bibliotecas utilizadas no download
 from PIL import Image
 import requests
 import io
 
-def getImagesURL(query:str, max_images:int, driver:webdriver, sleep_time = 1):
+
+def iniciarWebDriver(query:str, max_images:int):
     def scrollToEnd(driver, max_images):
         thumbnail_images = driver.find_elements_by_css_selector('img.Q4LuWd')
         number = 0
@@ -28,44 +32,58 @@ def getImagesURL(query:str, max_images:int, driver:webdriver, sleep_time = 1):
                     time.sleep(1)
             except:
                 continue
+    
+    
+    # Configurações do webdriver
+    option =  webdriver.FirefoxOptions()
+    option.add_argument("--no-sandbox")
+    option.add_argument("--disable-dev-shm-usage")
+    option.add_argument("--headless")
 
-
-    # Resultado da pesquisa
+    # Indo até o Google Imagens
+    driver = webdriver.Firefox(options=option)
     driver.get('https://www.google.com/imghp')
+
+    # Pesquisando
     search = driver.find_element_by_css_selector('input.gLFyf')
     button = driver.find_element_by_css_selector('button.Tg7LZd')
     search.send_keys(query)
     button.click()
 
-
-    urls = set()
-    count = 0
-
     # Indo até o fim da página
     scrollToEnd(driver, max_images)
+    return driver
+
+
+def getImagesURL(max_images:int, driver:webdriver, sleep_time = 1):
+    urls = list()
+    count = 0
 
     # Miniaturas das imagens
     thumbnail_images = driver.find_elements_by_css_selector('img.Q4LuWd')
     numbers_results = len(thumbnail_images)
-    print(f'Found {numbers_results} images. Getting links...')
+    print(f'{numbers_results} imagens encontradas. Pegando os links...')
 
     for image in thumbnail_images:
         image.click()
         time.sleep(sleep_time)
 
-        #Pegando link
+        # Pegando link
         actual_images = driver.find_elements_by_css_selector('img.n3VNCb')
         for actual_image in actual_images:
             if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                urls.add(actual_image.get_attribute('src'))
-                count += 1
+                if actual_image.get_attribute('src') not in urls:
+                    urls.append(actual_image.get_attribute('src'))
+                    count += 1
         
         if count >= max_images:
             break
     return urls
         
         
-def downloadImages(folder, file_names, url):
+def downloadImages(folder, file_names, urls):
+    qtd_imagens = 0
+
     if not os.path.exists(folder):
         os.makedirs(folder)
 
@@ -73,8 +91,11 @@ def downloadImages(folder, file_names, url):
         path = os.path.join(folder, file_names+f'{i:06d}'+'.jpg')
         try:
             downloadImage(path, url)
+            qtd_imagens += 1
         except:
             continue
+    
+    print(f'{qtd_imagens} imagens foram baixadas!')
 
 
 def downloadImage(path, url):
@@ -85,22 +106,18 @@ def downloadImage(path, url):
         save_image.save(file, 'JPEG', quality=85)
     
 
-if os.environ.get('DISPLAY','') == '':
-    os.environ.__setitem__('DISPLAY', ':0.0')
+def main():
+    if os.environ.get('DISPLAY','') == '':
+        os.environ.__setitem__('DISPLAY', ':0.0')
+
+    query = input('Query: ')
+    max_images = int(input('Múmero de imagens: '))
+    file_names = input('Nome que ira nos arquivos: ')
+    folder = input('Nome da pasta: ')
+
+    driver = iniciarWebDriver(query, max_images)
+    urls = getImagesURL(max_images, driver)
+    downloadImages(folder, file_names, urls)
 
 
-option =  webdriver.FirefoxOptions()
-option.add_argument("--no-sandbox")
-option.add_argument("--disable-dev-shm-usage")
-option.add_argument("--headless")
-
-WEBDRIVER_PATH = '/home/alexjr/Dev/google_img/geckodriver'
-driver = webdriver.Firefox(options=option)
-
-query = input('Query: ')
-max_images = int(input('Múmero de imagens: '))
-file_names = input('Nome que ira nos arquivos: ')
-folder = input('Nome da pasta: ')
-
-urls = getImagesURL(query, max_images, driver)
-downloadImages(folder, file_names, urls)
+main()
